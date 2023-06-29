d626a722ff8a6f01c55687ab4f0ad479e324b114
Remy102 <remysterling@outlook.com>
2023-06-29 22:54:50 +0300

    form unit test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SignupFormComponent } from './components/signup-form/signup-form.component';
-
-import { FormsModule } from '@angular/forms';
 // Import Angular Material modules
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
@@ -13,6 +11,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
--- src/app/components/signup-form/signup-form.component.spec.ts
+++ src/app/components/signup-form/signup-form.component.spec.ts
@@ -1,20 +1,88 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { SignupFormComponent } from './signup-form.component';
 
 describe('SignupFormComponent', () => {
   let component: SignupFormComponent;
   let fixture: ComponentFixture<SignupFormComponent>;
+  let httpMock: HttpTestingController;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FormsModule, HttpClientTestingModule],
+      declarations: [SignupFormComponent],
+    }).compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [SignupFormComponent]
-    });
     fixture = TestBed.createComponent(SignupFormComponent);
     component = fixture.componentInstance;
+    httpMock = TestBed.inject(HttpTestingController);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should update fullName when firstName and lastName are changed', () => {
+    component.firstName = 'John';
+    component.lastName = 'Doe';
+    component.updateFullName();
+    expect(component.fullName).toEqual('John Doe');
+  });
+
+  it('should make the first HTTP request with the correct URL', () => {
+    const lastNameLength = 3;
+    component.lastName = 'Doe';
+
+    component.makeFirstRequest(lastNameLength);
+
+    const request = httpMock.expectOne(`https://jsonplaceholder.typicode.com/photos/${lastNameLength}`);
+    expect(request.request.method).toBe('GET');
+  });
+
+  it('should make the second HTTP request with the correct data', () => {
+    const thumbnailUrl = 'https://example.com/thumbnail.jpg';
+    component.firstName = 'John';
+    component.lastName = 'Doe';
+    component.email = 'john@example.com';
+
+    component.makeSecondRequest(thumbnailUrl);
+
+    const expectedRequestBody = {
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john@example.com',
+      thumbnailUrl: thumbnailUrl,
+    };
+
+    const request = httpMock.expectOne('https://jsonplaceholder.typicode.com/users');
+    expect(request.request.method).toBe('POST');
+    expect(request.request.body).toEqual(expectedRequestBody);
+  });
+
+  it('should display a welcome message after successful form submission', () => {
+    spyOn(window, 'alert');
+    const form = {
+      invalid: false,
+    } as any;
+    component.firstName = 'John';
+    component.lastName = 'Doe';
+    component.email = 'john@example.com';
+
+    component.submitForm(form);
+
+    const thumbnailUrl = 'https://example.com/thumbnail.jpg';
+    const request = httpMock.expectOne(`https://jsonplaceholder.typicode.com/photos/${component.lastName.length}`);
+    request.flush({ thumbnailUrl });
+    const secondRequest = httpMock.expectOne('https://jsonplaceholder.typicode.com/users');
+    secondRequest.flush({});
+    expect(window.alert).toHaveBeenCalledWith('Welcome! You are successfully registered.');
   });
 });
